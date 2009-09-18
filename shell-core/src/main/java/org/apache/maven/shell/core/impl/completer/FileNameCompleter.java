42865f0d384f128f3ce986f17ca173a2b35f1198
Jason Dillon <jason@planet57.com>
2009-09-19 00:24:52 +0700

    Dupcliate most of the jline.FileNameCompletor to handle variables

--- shell-core/src/main/java/org/apache/maven/shell/core/impl/completer/FileNameCompleter.java
+++ shell-core/src/main/java/org/apache/maven/shell/core/impl/completer/FileNameCompleter.java
@@ -20,28 +20,92 @@
 package org.apache.maven.shell.core.impl.completer;
 
 import jline.Completor;
-import jline.FileNameCompletor;
+import org.apache.maven.shell.ShellHolder;
+import org.apache.maven.shell.VariableNames;
+import org.apache.maven.shell.Variables;
 import org.codehaus.plexus.component.annotations.Component;
 
+import java.io.File;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * {@link Completor} for file names.
  *
- * @version $Rev$ $Date$
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @version $Rev$ $Date$
  */
 @Component(role=Completor.class, hint="file-name")
 public class FileNameCompleter
-    implements Completor
+    implements Completor, VariableNames
 {
-    //
-    // FIXME: Need to use shell.user.dir etc for muck...
-    //
-    
-    private final FileNameCompletor delegate = new FileNameCompletor();
+   public int complete(final String buf, final int cursor, final List candidates) {
+        String buffer = (buf == null) ? "" : buf;
+        String translated = buffer;
 
-    public int complete(final String buffer, final int cursor, final List candidates) {
-        return delegate.complete(buffer, cursor, candidates);
+        Variables vars = ShellHolder.get().getVariables();
+        String home = vars.get(SHELL_USER_HOME, String.class);
+
+        // special character: ~ maps to the user's home directory
+        if (translated.startsWith("~" + File.separator)) {
+            translated = home + translated.substring(1);
+        }
+        else if (translated.startsWith("~")) {
+            translated = new File(home).getParentFile().getAbsolutePath();
+        }
+        else if (!(translated.startsWith(File.separator))) {
+            translated = new File("").getAbsolutePath() + File.separator + translated;
+        }
+
+        File file = new File(translated);
+
+        final File dir;
+
+        if (translated.endsWith(File.separator)) {
+            dir = file;
+        }
+        else {
+            dir = file.getParentFile();
+        }
+
+        final File[] entries = (dir == null) ? new File[0] : dir.listFiles();
+
+        try {
+            return matchFiles(buffer, translated, entries, candidates);
+        }
+        finally {
+            sortFileNames(candidates);
+        }
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    protected void sortFileNames(final List fileNames) {
+        Collections.sort(fileNames);
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public int matchFiles(final String buffer, final String translated, final File[] files, final List candidates) {
+        if (files == null) {
+            return -1;
+        }
+
+        int matches = 0;
+
+        // first pass: just count the matches
+        for (File file : files) {
+            if (file.getAbsolutePath().startsWith(translated)) {
+                matches++;
+            }
+        }
+        for (File file : files) {
+            if (file.getAbsolutePath().startsWith(translated)) {
+                String name = file.getName() + (((matches == 1) && file.isDirectory()) ? File.separator : " ");
+                candidates.add(name);
+            }
+        }
+
+        final int index = buffer.lastIndexOf(File.separator);
+
+        return index + File.separator.length();
     }
 }
