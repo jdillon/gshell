993eef24e75c33b163b548c01d0b647d9c7ec8dc
Jason Dillon <jason@planet57.com>
2009-09-18 23:29:16 +0700

    These are still internal FTM

--- shell-core/src/main/java/org/apache/maven/shell/core/ShellBuilder.java
+++ shell-core/src/main/java/org/apache/maven/shell/core/ShellBuilder.java
@@ -26,6 +26,7 @@ import org.apache.maven.shell.Variables;
 import org.apache.maven.shell.command.CommandExecutor;
 import org.apache.maven.shell.console.Console;
 import org.apache.maven.shell.core.impl.ShellImpl;
+import org.apache.maven.shell.core.impl.CommandRegistrar;
 import org.apache.maven.shell.core.impl.plexus.Slf4jLoggerManager;
 import org.apache.maven.shell.io.IO;
 import org.codehaus.plexus.ContainerConfiguration;
