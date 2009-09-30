1e70d4c49b8d34baf2b122a8e27d6e4aaba7dfb7
Jason Dillon <jason@planet57.com>
2009-09-30 18:16:40 +0700

    Adding info command from karaf

---
+++ gshell-core/src/main/java/org/apache/gshell/core/commands/InfoCommand.java
@@ -0,0 +1,184 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.gshell.core.commands;
+
+import org.apache.gshell.command.CommandSupport;
+import org.apache.gshell.command.CommandContext;
+import org.apache.gshell.io.IO;
+import org.apache.gshell.Branding;
+import org.apache.gshell.cli.Option;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.util.StringUtils;
+
+import java.util.Locale;
+import java.text.DecimalFormatSymbols;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ClassLoadingMXBean;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.ThreadMXBean;
+import java.lang.management.RuntimeMXBean;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.reflect.Method;
+
+/**
+ * Display shell information.
+ *
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ *
+ * @since 2.0
+ */
+@Component(role=InfoCommand.class)
+public class InfoCommand
+    extends CommandSupport
+{
+    private final NumberFormat fmtI = new DecimalFormat("###,###", new DecimalFormatSymbols(Locale.ENGLISH));
+
+    private final NumberFormat fmtD = new DecimalFormat("###,##0.000", new DecimalFormatSymbols(Locale.ENGLISH));
+
+    @Option(name="-v", aliases={"--verbose"})
+    private boolean verbose;
+
+    @Override
+    public Object execute(final CommandContext context) throws Exception {
+        assert context != null;
+        IO io = context.getIo();
+
+        int maxNameLen = 25;
+        Branding branding = context.getShell().getBranding();
+        RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
+        OperatingSystemMXBean os = ManagementFactory.getOperatingSystemMXBean();
+        ThreadMXBean threads = ManagementFactory.getThreadMXBean();
+        MemoryMXBean mem = ManagementFactory.getMemoryMXBean();
+        ClassLoadingMXBean cl = ManagementFactory.getClassLoadingMXBean();
+
+        //
+        // TODO: i18n all this
+        //
+
+        io.info("{} ({})", branding.getAboutMessage(), branding.getVersion());
+        io.out.println();
+        
+        io.out.println("Shell");
+        printValue(io, "Home", maxNameLen, System.getProperty("shell.home"));
+        printValue(io, "Display Name", maxNameLen, branding.getDisplayName());
+        printValue(io, "Program Name", maxNameLen, branding.getProgramName());
+        printValue(io, "Script Extension", maxNameLen, branding.getScriptExtension());
+        printValue(io, "Script Home Dir", maxNameLen, branding.getShellHomeDir());
+        printValue(io, "Script Context Dir", maxNameLen, branding.getShellContextDir());
+        printValue(io, "Script User Home Dir", maxNameLen, branding.getUserHomeDir());
+        printValue(io, "Script User Context Dir", maxNameLen, branding.getUserContextDir());
+
+        io.out.println("JVM");
+        printValue(io, "Java Virtual Machine", maxNameLen, runtime.getVmName() + " version " + runtime.getVmVersion());
+        printValue(io, "Vendor", maxNameLen, runtime.getVmVendor());
+        printValue(io, "Uptime", maxNameLen, printDuration(runtime.getUptime()));
+        try {
+            printValue(io, "Process CPU time", maxNameLen, printDuration(getSunOsValueAsLong(os, "getProcessCpuTime") / 1000000));
+        }
+        catch (Throwable t) {}
+        printValue(io, "Total compile time", maxNameLen, printDuration(ManagementFactory.getCompilationMXBean().getTotalCompilationTime()));
+
+        io.out.println("Threads");
+        printValue(io, "Live threads", maxNameLen, Integer.toString(threads.getThreadCount()));
+        printValue(io, "Daemon threads", maxNameLen, Integer.toString(threads.getDaemonThreadCount()));
+        printValue(io, "Peak", maxNameLen, Integer.toString(threads.getPeakThreadCount()));
+        printValue(io, "Total started", maxNameLen, Long.toString(threads.getTotalStartedThreadCount()));
+
+        io.out.println("Memory");
+        printValue(io, "Current heap size", maxNameLen, printSizeInKb(mem.getHeapMemoryUsage().getUsed()));
+        printValue(io, "Maximum heap size", maxNameLen, printSizeInKb(mem.getHeapMemoryUsage().getMax()));
+        printValue(io, "Committed heap size", maxNameLen, printSizeInKb(mem.getHeapMemoryUsage().getCommitted()));
+        printValue(io, "Pending objects", maxNameLen, Integer.toString(mem.getObjectPendingFinalizationCount()));
+        for (GarbageCollectorMXBean gc : ManagementFactory.getGarbageCollectorMXBeans()) {
+            String val = "Name = '" + gc.getName() + "', Collections = " + gc.getCollectionCount() + ", Time = " + printDuration(gc.getCollectionTime());
+            printValue(io, "Garbage collector", maxNameLen, val);
+        }
+
+        io.out.println("Classes");
+        printValue(io, "Current classes loaded", maxNameLen, printLong(cl.getLoadedClassCount()));
+        printValue(io, "Total classes loaded", maxNameLen, printLong(cl.getTotalLoadedClassCount()));
+        printValue(io, "Total classes unloaded", maxNameLen, printLong(cl.getUnloadedClassCount()));
+
+        io.out.println("Operating system");
+        printValue(io, "Name", maxNameLen, os.getName() + " version " + os.getVersion());
+        printValue(io, "Architecture", maxNameLen, os.getArch());
+        printValue(io, "Processors", maxNameLen, Integer.toString(os.getAvailableProcessors()));
+        try {
+            printValue(io, "Total physical memory", maxNameLen, printSizeInKb(getSunOsValueAsLong(os, "getTotalPhysicalMemorySize")));
+            printValue(io, "Free physical memory", maxNameLen, printSizeInKb(getSunOsValueAsLong(os, "getFreePhysicalMemorySize")));
+            printValue(io, "Committed virtual memory", maxNameLen, printSizeInKb(getSunOsValueAsLong(os, "getCommittedVirtualMemorySize")));
+            printValue(io, "Total swap space", maxNameLen, printSizeInKb(getSunOsValueAsLong(os, "getTotalSwapSpaceSize")));
+            printValue(io, "Free swap space", maxNameLen, printSizeInKb(getSunOsValueAsLong(os, "getFreeSwapSpaceSize")));
+        } catch (Throwable t) {}
+
+        return null;
+    }
+
+    private long getSunOsValueAsLong(OperatingSystemMXBean os, String name) throws Exception {
+        Method mth = os.getClass().getMethod(name);
+        return (Long) mth.invoke(os);
+    }
+
+    private String printLong(long i) {
+        return fmtI.format(i);
+    }
+
+    private String printSizeInKb(double size) {
+        return fmtI.format((long) (size / 1024)) + " kbytes";
+    }
+
+    private String printDuration(double uptime) {
+        uptime /= 1000;
+        if (uptime < 60) {
+            return fmtD.format(uptime) + " seconds";
+        }
+        uptime /= 60;
+        if (uptime < 60) {
+            long minutes = (long) uptime;
+            String s = fmtI.format(minutes) + (minutes > 1 ? " minutes" : " minute");
+            return s;
+        }
+        uptime /= 60;
+        if (uptime < 24) {
+            long hours = (long) uptime;
+            long minutes = (long) ((uptime - hours) * 60);
+            String s = fmtI.format(hours) + (hours > 1 ? " hours" : " hour");
+            if (minutes != 0) {
+                s += " " + fmtI.format(minutes) + (minutes > 1 ? " minutes" : "minute");
+            }
+            return s;
+        }
+        uptime /= 24;
+        long days = (long) uptime;
+        long hours = (long) ((uptime - days) * 60);
+        String s = fmtI.format(days) + (days > 1 ? " days" : " day");
+        if (hours != 0) {
+            s += " " + fmtI.format(hours) + (hours > 1 ? " hours" : "hour");
+        }
+        return s;
+    }
+
+    private void printValue(final IO io, final String name, final int pad, final Object value) {
+        io.out.format("  @|bold %s|%s    %s", name, StringUtils.repeat(" ", pad - name.length()), value).println();
+    }
+}
