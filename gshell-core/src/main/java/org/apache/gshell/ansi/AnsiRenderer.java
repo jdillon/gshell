333e8d6a299c765a86bf9bd5a6eed6ff0dd23366
Jason Dillon <jason@planet57.com>
2009-10-07 21:12:16 +0700

    Move Code back to AnsiRenderer

--- gshell-core/src/main/java/org/apache/gshell/ansi/AnsiRenderer.java
+++ gshell-core/src/main/java/org/apache/gshell/ansi/AnsiRenderer.java
@@ -19,6 +19,8 @@
 
 package org.apache.gshell.ansi;
 
+import org.fusesource.jansi.Ansi;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -68,7 +70,7 @@ public class AnsiRenderer
         org.fusesource.jansi.Ansi ansi = Ansi.ansi();
 
         for (String name : codes) {
-            Ansi.Code code = Ansi.Code.valueOf(name.toUpperCase());
+            Code code = Code.valueOf(name.toUpperCase());
 
             if (code.isColor()) {
                 if (code.isBackground()) {
@@ -88,5 +90,93 @@ public class AnsiRenderer
 
     public static boolean test(final String text) {
         return text != null && text.contains(BEGIN_TOKEN);
+    }
+
+    public static enum Code
+    {
+        // Colors
+        BLACK(org.fusesource.jansi.Ansi.Color.BLACK),
+        RED(Ansi.Color.RED),
+        GREEN(Ansi.Color.GREEN),
+        YELLOW(Ansi.Color.YELLOW),
+        BLUE(Ansi.Color.BLUE),
+        MAGENTA(Ansi.Color.MAGENTA),
+        CYAN(Ansi.Color.CYAN),
+        WHITE(Ansi.Color.WHITE),
+
+        // Forground Colors
+        FG_BLACK(Ansi.Color.BLACK, false),
+        FG_RED(Ansi.Color.RED, false),
+        FG_GREEN(Ansi.Color.GREEN, false),
+        FG_YELLOW(Ansi.Color.YELLOW, false),
+        FG_BLUE(Ansi.Color.BLUE, false),
+        FG_MAGENTA(Ansi.Color.MAGENTA, false),
+        FG_CYAN(Ansi.Color.CYAN, false),
+        FG_WHITE(Ansi.Color.WHITE, false),
+
+        // Background Colors
+        BG_BLACK(Ansi.Color.BLACK, true),
+        BG_RED(Ansi.Color.RED, true),
+        BG_GREEN(Ansi.Color.GREEN, true),
+        BG_YELLOW(Ansi.Color.YELLOW, true),
+        BG_BLUE(Ansi.Color.BLUE, true),
+        BG_MAGENTA(Ansi.Color.MAGENTA, true),
+        BG_CYAN(Ansi.Color.CYAN, true),
+        BG_WHITE(Ansi.Color.WHITE, true),
+
+        // Attributes
+        RESET(Ansi.Attribute.RESET),
+        INTENSITY_BOLD(Ansi.Attribute.INTENSITY_BOLD),
+        INTENSITY_FAINT(Ansi.Attribute.INTENSITY_FAINT),
+        ITALIC(Ansi.Attribute.ITALIC),
+        UNDERLINE(Ansi.Attribute.UNDERLINE),
+        BLINK_SLOW(Ansi.Attribute.BLINK_SLOW),
+        BLINK_FAST(Ansi.Attribute.BLINK_FAST),
+        BLINK_OFF(Ansi.Attribute.BLINK_OFF),
+        NEGATIVE_ON(Ansi.Attribute.NEGATIVE_ON),
+        NEGATIVE_OFF(Ansi.Attribute.NEGATIVE_OFF),
+        CONCEAL_ON(Ansi.Attribute.CONCEAL_ON),
+        CONCEAL_OFF(Ansi.Attribute.CONCEAL_OFF),
+        UNDERLINE_DOUBLE(Ansi.Attribute.UNDERLINE_DOUBLE),
+        INTENSITY_NORMAL(Ansi.Attribute.INTENSITY_NORMAL),
+        UNDERLINE_OFF(Ansi.Attribute.UNDERLINE_OFF),
+
+        // Aliases
+        BOLD(Ansi.Attribute.INTENSITY_BOLD),
+        FAINT(Ansi.Attribute.INTENSITY_FAINT),
+        ;
+
+        private final Enum n;
+
+        private final boolean background;
+
+        private Code(final Enum n, boolean background) {
+            this.n = n;
+            this.background = background;
+        }
+
+        private Code(final Enum n) {
+            this(n, false);
+        }
+
+        public boolean isColor() {
+            return n instanceof Ansi.Color;
+        }
+
+        public Ansi.Color getColor() {
+            return (Ansi.Color) n;
+        }
+
+        public boolean isAttribute() {
+            return n instanceof Ansi.Attribute;
+        }
+
+        public Ansi.Attribute getAttribute() {
+            return (Ansi.Attribute) n;
+        }
+
+        public boolean isBackground() {
+            return background;
+        }
     }
 }
