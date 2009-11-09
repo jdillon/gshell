a39c7ca8170cbabd5a769d89dbe9865e32a20be2
Jason Dillon <jason@planet57.com>
2009-11-09 15:14:23 +0700

    Add ctrl-c handling based on impl from Apache Karaf

--- gshell-core/src/main/java/org/sonatype/gshell/core/console/ConsoleImpl.java
+++ gshell-core/src/main/java/org/sonatype/gshell/core/console/ConsoleImpl.java
@@ -16,6 +16,7 @@
 
 package org.sonatype.gshell.core.console;
 
+import jline.Terminal;
 import jline.console.CandidateListCompletionHandler;
 import jline.console.Completer;
 import jline.console.ConsoleReader;
@@ -26,7 +27,13 @@ import org.sonatype.gshell.console.Console;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+// Based on Apache Karaf impl
 
 /**
  * Support for running console using the <a href="http://jline.sf.net">JLine</a> library.
@@ -39,19 +46,30 @@ public class ConsoleImpl
 {
     private final ConsoleReader reader;
 
+    private final Thread pipe;
+
+    private final BlockingQueue<Integer> queue;
+
+    private boolean interrupt;
+
     public ConsoleImpl(final Executor executor, final IO io, final History history, final InputStream bindings) throws IOException {
         super(executor);
         assert io != null;
 
-        reader = new ConsoleReader(
-            io.streams.in,
-            new PrintWriter(io.streams.out, true),
+        this.queue = new ArrayBlockingQueue<Integer>(1024);
+        this.pipe = new Thread(new Pipe(io));
+        this.pipe.setName("Console Pipe");
+        this.pipe.setDaemon(true);
+
+        this.reader = new ConsoleReader(
+            new ConsoleInputStream(),
+            new PrintWriter(io.streams.out),
             bindings,
             io.getTerminal());
 
-        reader.setPaginationEnabled(true);
-        reader.setCompletionHandler(new CandidateListCompletionHandler());
-        reader.setHistory(history != null ? history : new MemoryHistory());
+        this.reader.setPaginationEnabled(true);
+        this.reader.setCompletionHandler(new CandidateListCompletionHandler());
+        this.reader.setHistory(history != null ? history : new MemoryHistory());
     }
 
     public ConsoleImpl(final Executor executor, final IO io) throws IOException {
@@ -71,5 +89,154 @@ public class ConsoleImpl
     protected String readLine(final String prompt) throws IOException {
         // prompt may be null
         return reader.readLine(prompt);
+    }
+
+    @Override
+    public void run() {
+        pipe.start();
+        super.run();
+    }
+
+    private void checkInterrupt() throws IOException {
+        if (interrupt) {
+            interrupt = false;
+            throw new InterruptedIOException("Keyboard interruption");
+        }
+    }
+
+    private void interrupt() {
+        log.debug("Interrupted");
+        interrupt = true;
+    }
+
+    private void close() {
+        log.debug("Closing");
+        this.setRunning(false);
+        pipe.interrupt();
+        Thread.interrupted();
+    }
+
+    private class ConsoleInputStream
+        extends InputStream
+    {
+        private int read(final boolean wait) throws IOException {
+            if (!running) {
+                return -1;
+            }
+            checkInterrupt();
+            Integer i;
+            if (wait) {
+                try {
+                    i = queue.take();
+                }
+                catch (InterruptedException e) {
+                    throw new InterruptedIOException();
+                }
+                checkInterrupt();
+            }
+            else {
+                i = queue.poll();
+            }
+            if (i == null) {
+                return -1;
+            }
+            return i;
+        }
+
+        @Override
+        public int read() throws IOException {
+            return read(true);
+        }
+
+        @Override
+        public int read(final byte b[], int off, final int len) throws IOException {
+            if (b == null) {
+                throw new NullPointerException();
+            }
+            else if (off < 0 || len < 0 || len > b.length - off) {
+                throw new IndexOutOfBoundsException();
+            }
+            else if (len == 0) {
+                return 0;
+            }
+
+            int nb = 1;
+            int i = read(true);
+            if (i < 0) {
+                return -1;
+            }
+            b[off++] = (byte) i;
+            while (nb < len) {
+                i = read(false);
+                if (i < 0) {
+                    return nb;
+                }
+                b[off++] = (byte) i;
+                nb++;
+            }
+            return nb;
+        }
+    }
+
+    private class Pipe
+        implements Runnable
+    {
+        private final Terminal term;
+
+        private final InputStream in;
+
+        private final PrintStream err;
+
+        private Pipe(final IO io) {
+            assert io != null;
+            this.term = io.getTerminal();
+            this.in = io.streams.in;
+            this.err = io.streams.err;
+        }
+
+        private int read() throws IOException {
+            // FIXME: See if this is really needed and figure out why...
+//            if (term instanceof AnsiWindowsTerminal) {
+//                c = ((AnsiWindowsTerminal) term).readDirectChar(in);
+//            }
+//            else {
+//                c = terminal.readCharacter(in);
+//            }
+            return term.readCharacter(in);
+        }
+
+        public void run() {
+            try {
+                while (running) {
+                    try {
+                        int c = read();
+
+                        switch (c) {
+                            case -1:
+                                queue.put(c);
+                                return;
+
+                            case 3:
+                                err.println("^C");
+                                reader.getCursorBuffer().clear();
+                                interrupt();
+                                break;
+
+                            case 4:
+                                err.println("^D");
+                                break;
+                        }
+
+                        queue.put(c);
+                    }
+                    catch (Throwable t) {
+                        return;
+                    }
+                }
+            }
+            finally {
+                close();
+            }
+        }
     }
 }
