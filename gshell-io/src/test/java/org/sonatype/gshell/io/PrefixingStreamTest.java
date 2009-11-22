61cdc0fd2e031b5e337de5ad449582b35b2931c4
Jason Dillon <jason@planet57.com>
2009-11-22 16:57:16 +0700

    Add test for PrefixingStream

---
+++ gshell-io/src/test/java/org/sonatype/gshell/io/PrefixingStreamTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2009 the original author(s).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sonatype.gshell.io;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Unit tests for the {@link PrefixingStream} class.
+ *
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ */
+public class PrefixingStreamTest
+{
+    @Test
+    public void testStream() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrefixingStream prefixed = new PrefixingStream("TEST", baos);
+        PrintStream out = new PrintStream(prefixed);
+
+        out.println("HI");
+        out.println("there");
+        out.print("foo");
+        out.print("bar");
+        out.println("baz");
+        out.flush();
+
+        String tmp = new String(baos.toByteArray());
+        System.out.println(tmp);
+
+        String expected = "TESTHI\n" +
+                "TESTthere\n" +
+                "TESTfoobarbaz\n";
+
+        assertEquals(expected, tmp);
+    }
+}
