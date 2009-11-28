3930281d581cd0f5ac0f261e13446a26d3b00596
Jason Dillon <jason@planet57.com>
2009-11-28 13:18:49 +0700

    Make InputPipe a Closeable

--- gshell-io/src/main/java/org/sonatype/gshell/io/InputPipe.java
+++ gshell-io/src/main/java/org/sonatype/gshell/io/InputPipe.java
@@ -20,6 +20,7 @@ import jline.Terminal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InterruptedIOException;
@@ -35,6 +36,7 @@ import java.util.concurrent.CountDownLatch;
  */
 public class InputPipe
     extends Thread
+    implements Closeable
 {
     private static final Logger log = LoggerFactory.getLogger(InputPipe.class);
 
