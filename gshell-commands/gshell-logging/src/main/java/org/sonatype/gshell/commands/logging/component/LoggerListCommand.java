f65ba8e9096bf87774d9e1e861199c804463a19f
Jason Dillon <jason@planet57.com>
2010-02-11 18:44:46 +0700

    Repackage

--- gshell-commands/gshell-logging/src/main/java/org/sonatype/gshell/commands/logging/component/LoggerListCommand.java
+++ gshell-commands/gshell-logging/src/main/java/org/sonatype/gshell/commands/logging/component/LoggerListCommand.java
@@ -14,49 +14,62 @@
  * limitations under the License.
  */
 
-package org.sonatype.gshell.commands.logging;
+package org.sonatype.gshell.commands.logging.component;
 
 import com.google.inject.Inject;
 import org.sonatype.gshell.command.Command;
 import org.sonatype.gshell.command.CommandContext;
+import org.sonatype.gshell.command.IO;
 import org.sonatype.gshell.command.support.CommandActionSupport;
-import org.sonatype.gshell.logging.Level;
 import org.sonatype.gshell.logging.Logger;
 import org.sonatype.gshell.logging.LoggingSystem;
 import org.sonatype.gshell.util.cli2.Argument;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * Set the level of a logger.
+ * List loggers.
  *
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  * @since 2.5
  */
-@Command(name="logging/logger/set")
-public class LoggerSetLevelCommand
+@Command(name="logging/logger/list")
+public class LoggerListCommand
     extends CommandActionSupport
 {
     private final LoggingSystem logging;
 
-    @Argument(index=0, required=true)
-    private String loggerName;
-
-    @Argument(index=1, required=true)
-    private String levelName;
+    //
+    // TODO: Add level and name query as options, not arguments.  Include support for ^foo and foo^ for starts with/ends with.
+    //
+    
+    @Argument
+    private String nameQuery;
 
     @Inject
-    public LoggerSetLevelCommand(final LoggingSystem logging) {
+    public LoggerListCommand(final LoggingSystem logging) {
         assert logging != null;
         this.logging = logging;
     }
 
     public Object execute(final CommandContext context) throws Exception {
         assert context != null;
+        IO io = context.getIo();
 
-        Logger logger = logging.getLogger(loggerName);
-        Level level = logging.getLevel(levelName);
-        logger.setLevel(level);
-
-        log.debug("Set logger {} level to: {}", logger, level);
+        List<String> names = new ArrayList<String>();
+        names.addAll(logging.getLoggerNames());
+        Collections.sort(names);
+        
+        for (String name : names) {
+            if (nameQuery == null || name.contains(nameQuery)) {
+                Logger logger = logging.getLogger(name);
+                if (logger.getLevel() != null) {
+                    io.info("{}: {}", logger.getName(), logger.getLevel());
+                }
+            }
+        }
 
         return Result.SUCCESS;
     }
--- gshell-commands/gshell-logging/src/main/java/org/sonatype/gshell/commands/logging/component/LoggerSetLevelCommand.java
+++ gshell-commands/gshell-logging/src/main/java/org/sonatype/gshell/commands/logging/component/LoggerSetLevelCommand.java
@@ -14,62 +14,49 @@
  * limitations under the License.
  */
 
-package org.sonatype.gshell.commands.logging;
+package org.sonatype.gshell.commands.logging.component;
 
 import com.google.inject.Inject;
 import org.sonatype.gshell.command.Command;
 import org.sonatype.gshell.command.CommandContext;
-import org.sonatype.gshell.command.IO;
 import org.sonatype.gshell.command.support.CommandActionSupport;
+import org.sonatype.gshell.logging.Level;
 import org.sonatype.gshell.logging.Logger;
 import org.sonatype.gshell.logging.LoggingSystem;
 import org.sonatype.gshell.util.cli2.Argument;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 /**
- * List loggers.
+ * Set the level of a logger.
  *
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  * @since 2.5
  */
-@Command(name="logging/logger/list")
-public class LoggerListCommand
+@Command(name="logging/logger/set")
+public class LoggerSetLevelCommand
     extends CommandActionSupport
 {
     private final LoggingSystem logging;
 
-    //
-    // TODO: Add level and name query as options, not arguments.  Include support for ^foo and foo^ for starts with/ends with.
-    //
-    
-    @Argument
-    private String nameQuery;
+    @Argument(index=0, required=true)
+    private String loggerName;
+
+    @Argument(index=1, required=true)
+    private String levelName;
 
     @Inject
-    public LoggerListCommand(final LoggingSystem logging) {
+    public LoggerSetLevelCommand(final LoggingSystem logging) {
         assert logging != null;
         this.logging = logging;
     }
 
     public Object execute(final CommandContext context) throws Exception {
         assert context != null;
-        IO io = context.getIo();
 
-        List<String> names = new ArrayList<String>();
-        names.addAll(logging.getLoggerNames());
-        Collections.sort(names);
-        
-        for (String name : names) {
-            if (nameQuery == null || name.contains(nameQuery)) {
-                Logger logger = logging.getLogger(name);
-                if (logger.getLevel() != null) {
-                    io.info("{}: {}", logger.getName(), logger.getLevel());
-                }
-            }
-        }
+        Logger logger = logging.getLogger(loggerName);
+        Level level = logging.getLevel(levelName);
+        logger.setLevel(level);
+
+        log.debug("Set logger {} level to: {}", logger, level);
 
         return Result.SUCCESS;
     }
