c17ffd10412e16e5a0605af4d7369bcf531aee4a
Jason Dillon <jason@planet57.com>
2011-03-07 15:33:07 -0800

    Use GuiceMainSupport, depend on gshell-gossip

--- gshell-maven-plugin/src/main/java/org/sonatype/gshell/maven/ShellRunner.java
+++ gshell-maven-plugin/src/main/java/org/sonatype/gshell/maven/ShellRunner.java
@@ -16,23 +16,17 @@
 package org.sonatype.gshell.maven;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.google.inject.Stage;
-import com.google.inject.name.Names;
-import org.sonatype.gshell.MainSupport;
 import org.sonatype.gshell.branding.Branding;
-import org.sonatype.gshell.command.IO;
-import org.sonatype.gshell.command.registry.CommandRegistrar;
 import org.sonatype.gshell.console.ConsoleErrorHandler;
 import org.sonatype.gshell.console.ConsolePrompt;
-import org.sonatype.gshell.guice.CoreModule;
-import org.sonatype.gshell.shell.Shell;
+import org.sonatype.gshell.guice.GuiceMainSupport;
+import org.sonatype.gshell.logging.LoggingSystem;
+import org.sonatype.gshell.logging.gossip.GossipLoggingSystem;
 import org.sonatype.gshell.shell.ShellErrorHandler;
-import org.sonatype.gshell.shell.ShellImpl;
 import org.sonatype.gshell.shell.ShellPrompt;
-import org.sonatype.gshell.variables.Variables;
+
+import java.util.List;
 
 /**
  * Runs GShell for use in a maven-plugin.
@@ -40,7 +34,7 @@ import org.sonatype.gshell.variables.Variables;
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  */
 public class ShellRunner
-    extends MainSupport
+    extends GuiceMainSupport
 {
     @Override
     protected Branding createBranding() {
@@ -48,25 +42,19 @@ public class ShellRunner
     }
 
     @Override
-    protected Shell createShell() throws Exception {
-        // FIXME: Share more with Main ...
-        
-        Module module = new AbstractModule()
+    protected void configure(final List<Module> modules) {
+        super.configure(modules);
+
+        Module custom = new AbstractModule()
         {
             @Override
             protected void configure() {
+                bind(LoggingSystem.class).to(GossipLoggingSystem.class);
                 bind(ConsolePrompt.class).to(ShellPrompt.class);
                 bind(ConsoleErrorHandler.class).to(ShellErrorHandler.class);
-                bind(Branding.class).toInstance(getBranding());
-                bind(IO.class).annotatedWith(Names.named("main")).toInstance(io);
-                bind(Variables.class).annotatedWith(Names.named("main")).toInstance(vars);
             }
         };
 
-        Injector injector = Guice.createInjector(Stage.PRODUCTION, module, new CoreModule());
-        ShellImpl shell = injector.getInstance(ShellImpl.class);
-        injector.getInstance(CommandRegistrar.class).registerCommands();
-
-        return shell;
+        modules.add(custom);
     }
 }
