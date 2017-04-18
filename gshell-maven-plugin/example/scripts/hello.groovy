61970c40545d99ddfdf465df4efafdccc652d8b4
Jason Dillon <jason@planet57.com>
2017-04-17 19:38:24 -0700

    notes

--- gshell-maven-plugin/example/scripts/hello.groovy
+++ gshell-maven-plugin/example/scripts/hello.groovy
@@ -13,4 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// This is a simple test script which is configured with an alias 'hello' and
+// setup to run automatically for interactive shells.
+
 println "Hello!"
