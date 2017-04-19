31b574363b317bc478f8cc13989ca4e0ffdc7174
Jason Dillon <jason@planet57.com>
2017-04-18 20:30:07 -0700

    simplify

--- gshell-core/src/main/java/com/planet57/gshell/console/FileNameCompleter.java
+++ gshell-core/src/main/java/com/planet57/gshell/console/FileNameCompleter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.planet57.gshell.console.completer;
+package com.planet57.gshell.console;
 
 import java.io.File;
 
