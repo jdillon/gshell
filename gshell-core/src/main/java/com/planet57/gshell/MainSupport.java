48369278ef9c583a3621203b13f2853331a41b6c
Jason Dillon <jason@planet57.com>
2017-04-28 20:33:58 -0700

    simplify

--- gshell-core/src/main/java/com/planet57/gshell/MainSupport.java
+++ gshell-core/src/main/java/com/planet57/gshell/MainSupport.java
@@ -217,16 +217,17 @@ public abstract class MainSupport
     });
   }
 
-  public void boot(String... args) throws Exception {
+  public void boot(final String... args) throws Exception {
     checkNotNull(args);
 
-    args = cleanArguments(args);
     log.debug("Booting w/args: {}", Arrays.asList(args));
 
     // Register default handler for uncaught exceptions
     Thread.setDefaultUncaughtExceptionHandler((thread, cause) -> log.warn("Unhandled exception occurred on thread: " + thread, cause));
 
     Terminal terminal = TerminalBuilder.builder()
+      .system(true)
+      .nativeSignals(true)
       .build();
 
     io = new IO(createStreamSet(), terminal);
@@ -261,7 +262,7 @@ public abstract class MainSupport
     }
 
     if (help) {
-      HelpPrinter printer = new HelpPrinter(clp, io.terminal);
+      HelpPrinter printer = new HelpPrinter(clp, terminal);
       printer.printUsage(io.out, getBranding().getProgramName());
       exit(0);
     }
@@ -271,7 +272,7 @@ public abstract class MainSupport
       exit(0);
     }
 
-    // adapt JUL and force slf4j backend to initialize
+    // adapt JUL
     SLF4JBridgeHandler.removeHandlersForRootLogger();
     SLF4JBridgeHandler.install();
 
@@ -279,7 +280,6 @@ public abstract class MainSupport
     StreamJack.maybeInstall(io.streams);
 
     Object result = null;
-
     try {
       variables.set(VariableNames.SHELL_ERRORS, showErrorTraces);
 
@@ -310,80 +310,5 @@ public abstract class MainSupport
     }
 
     exit(ExitCodeDecoder.decode(result));
-  }
-
-  private static String[] cleanArguments(final String[] args) {
-    List<String> cleaned = new ArrayList<>();
-
-    StringBuilder currentArg = null;
-
-    for (int i = 0; i < args.length; i++) {
-      String arg = args[i];
-
-      boolean addedToBuffer = false;
-
-      if (arg.startsWith("\"")) {
-        // if we're in the process of building up another arg, push it and start over.
-        // this is for the case: "-Dfoo=bar "-Dfoo2=bar two" (note the first unterminated quote)
-        if (currentArg != null) {
-          cleaned.add(currentArg.toString());
-        }
-
-        // start building an argument here.
-        currentArg = new StringBuilder(arg.substring(1));
-        addedToBuffer = true;
-      }
-
-      // this has to be a separate "if" statement, to capture the case of: "-Dfoo=bar"
-      if (arg.endsWith("\"")) {
-        String cleanArgPart = arg.substring(0, arg.length() - 1);
-
-        // if we're building an argument, keep doing so.
-        if (currentArg != null) {
-          // if this is the case of "-Dfoo=bar", then we need to adjust the buffer.
-          if (addedToBuffer) {
-            currentArg.setLength(currentArg.length() - 1);
-          }
-          // otherwise, we trim the trailing " and append to the buffer.
-          else {
-            // TODO: introducing a space here...not sure what else to do but collapse whitespace
-            currentArg.append(' ').append(cleanArgPart);
-          }
-
-          cleaned.add(currentArg.toString());
-        }
-        else {
-          cleaned.add(cleanArgPart);
-        }
-
-        currentArg = null;
-
-        continue;
-      }
-
-      // if we haven't added this arg to the buffer, and we ARE building an argument
-      // buffer, then append it with a preceding space...again, not sure what else to
-      // do other than collapse whitespace.
-      // NOTE: The case of a trailing quote is handled by nullifying the arg buffer.
-      if (!addedToBuffer) {
-        if (currentArg != null) {
-          currentArg.append(' ').append(arg);
-        }
-        else {
-          cleaned.add(arg);
-        }
-      }
-    }
-
-    if (currentArg != null) {
-      cleaned.add(currentArg.toString());
-    }
-
-    if (cleaned.isEmpty()) {
-      return args;
-    }
-    else {
-      return cleaned.toArray(new String[cleaned.size()]);
-    }
   }
 }
