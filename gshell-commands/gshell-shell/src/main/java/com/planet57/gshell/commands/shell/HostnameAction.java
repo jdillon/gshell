b1cde09c712fa8c2f532c72f65b4fb82e5542e5f
Jason Dillon <jason@planet57.com>
2017-04-28 21:34:49 -0700

    merge gshell-network into gshell-shell for now

--- gshell-commands/gshell-shell/src/main/java/com/planet57/gshell/commands/shell/HostnameAction.java
+++ gshell-commands/gshell-shell/src/main/java/com/planet57/gshell/commands/shell/HostnameAction.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.planet57.gshell.commands.network;
+package com.planet57.gshell.commands.shell;
 
 import java.net.InetAddress;
 
--- gshell-commands/gshell-shell/src/main/java/com/planet57/gshell/commands/shell/WgetAction.java
+++ gshell-commands/gshell-shell/src/main/java/com/planet57/gshell/commands/shell/WgetAction.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.planet57.gshell.commands.network;
+package com.planet57.gshell.commands.shell;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
