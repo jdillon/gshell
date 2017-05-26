58badc10a9384c8db27004e2fbb454ec588000d8
Jason Dillon <jason@planet57.com>
2017-05-26 15:09:16 -0700

    Add basic test for ArgumentDescriptor

---
+++ gshell-util/src/test/java/com/planet57/gshell/util/cli2/ArgumentDescriptorTest.groovy
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2009-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.planet57.gshell.util.cli2
+
+import java.lang.annotation.Annotation
+
+import org.sonatype.goodies.testsupport.TestSupport
+
+import com.planet57.gshell.util.cli2.handler.Handler
+import org.junit.Test
+
+/**
+ * Tests for {@link ArgumentDescriptor}.
+ */
+class ArgumentDescriptorTest
+  extends TestSupport
+{
+  @Test
+  void 'test render-syntax with default token'() {
+    def desc = new Argument() {
+      @Override
+      int index() {
+        return 0
+      }
+
+      @Override
+      String token() {
+        return null
+      }
+
+      @Override
+      boolean required() {
+        return false
+      }
+
+      @Override
+      String description() {
+        return null
+      }
+
+      @Override
+      Class<? extends Handler> handler() {
+        return null
+      }
+
+      @Override
+      Class<? extends Annotation> annotationType() {
+        return null
+      }
+    }
+    def underTest = new ArgumentDescriptor(desc, new NopSetter())
+    assert underTest.getToken() == 'ARG'
+  }
+
+  @Test
+  void 'test render-syntax with specified token'() {
+    def desc = new Argument() {
+      @Override
+      int index() {
+        return 0
+      }
+
+      @Override
+      String token() {
+        return 'FOO'
+      }
+
+      @Override
+      boolean required() {
+        return false
+      }
+
+      @Override
+      String description() {
+        return null
+      }
+
+      @Override
+      Class<? extends Handler> handler() {
+        return null
+      }
+
+      @Override
+      Class<? extends Annotation> annotationType() {
+        return null
+      }
+    }
+    def underTest = new ArgumentDescriptor(desc, new NopSetter())
+    assert underTest.getToken() == 'FOO'
+  }
+}
